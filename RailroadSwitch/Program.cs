5c912c45852c94d8f0df0d808f1b0cbd5a5584ea
Aydin Mir Mohammadi <am@bluehands.de>
2026-05-14 19:32:57 +0200

    Refactor II

--- RailroadSwitch/Program.cs
+++ RailroadSwitch/Program.cs
@@ -5,14 +5,7 @@ var cmd = new SetCommand(signingCert, SwitchDirection.Left);
 
 var railroadSwitch = new RailroadSwitch();
 var errorMessage = railroadSwitch.Set(cmd);
-if (string.IsNullOrEmpty(errorMessage))
-{
-    Console.WriteLine("Successfully set the switch");
-}
-else
-{
-    Console.WriteLine($"Error set the switch: {errorMessage}");
-}
 
+Console.WriteLine(string.IsNullOrEmpty(errorMessage) ? "Successfully set the switch" : $"Error set the switch: {errorMessage}");
 Console.WriteLine("Press [ENTER] to exit");
 Console.ReadLine();
