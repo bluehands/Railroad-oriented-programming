6fda3e520bfbfd120e392bf35c64aefc3eaa1f13
Aydin Mir Mohammadi <am@bluehands.de>
2026-05-15 17:35:28 +0200

    print error message

--- RailroadSwitch/Program.cs
+++ RailroadSwitch/Program.cs
@@ -6,7 +6,7 @@ var cmd = new SetCommand(signingCert, SwitchDirection.Left);
 var railroadSwitch = new RailroadSwitch();
 railroadSwitch.Set(cmd).Match(
     _=> Console.WriteLine("Successfully set the switch"),
-    e=> Console.WriteLine($"Error set the switch: {e}")
+    e=> Console.WriteLine($"Error set the switch: {e.Message}")
     );
 
 Console.WriteLine("Press [ENTER] to exit");
