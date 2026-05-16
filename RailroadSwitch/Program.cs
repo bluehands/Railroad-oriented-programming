c9df66a785b080e591c55f0c1c4671559dd5a589
Aydin Mir Mohammadi <am@bluehands.de>
2026-05-16 18:01:37 +0200

    use it from args

--- RailroadSwitch/Program.cs
+++ RailroadSwitch/Program.cs
@@ -1,10 +1,14 @@
 ﻿using RailroadSwitchGateway;
 
 var signingCert = args.GetSigningCert();
-var cmd = new SetCommand(signingCert, SwitchDirection.Left);
+var switchDirection = args.GetSwitchDirection();
+
+
+var cmd = new SetCommand(signingCert, switchDirection);
 
 var railroadSwitch = new RailroadSwitch();
 var errorMessage = railroadSwitch.Set(cmd);
+
 
 Console.WriteLine(string.IsNullOrEmpty(errorMessage) ? "Successfully set the switch" : $"Error set the switch: {errorMessage}");
 Console.WriteLine("Press [ENTER] to exit");
